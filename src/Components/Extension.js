cc1a62293e8516abbbb9495c28000eb58a71b5f0
tejtilak <tejtilak01@gmail.com>
2022-10-10 12:25:20 +0530

    first-update

--- src/Components/Extension.js
+++ src/Components/Extension.js
@@ -1,28 +1,28 @@
 import React from 'react'
 
-const Extension = ({data}) => {
-  return (
-    <div>
-        <div className='row'>
-            {data.map(data =>
-            <div className='col-md-4'>
-                <div class='card' style={{"width": "18ren"}}>
-                    <img class="card-img-top" src={data.recipe.image} alt="Card img cap" />
-                    <div class="card-body">
+const Extension = ({ data }) => {
+    return (
+        <div>
+            <div className='row'>
+                {data.map(data =>
+                    <div className='col-md-4'>
+                        <div class='card' style={{ "width": "18ren" }}>
+                            <img class="card-img-top" src={data.recipe.image} alt="Card img cap" />
+                            <div class="card-body">
 
-                        <center>
-                            <h5 class="card-title">{data.recipe.label}</h5>
-                            <p class="card-text">Total Amount of calories: {Math.round(data.recipe.calories)} </p>
-                            <a href='https://www.edamam.com/results/recipes/?search=all'  class="btn btn-primary">Buy</a>
-                        </center>
-                    </div>    
-                </div>                
+                                <center>
+                                    <h5 class="card-title">{data.recipe.label}</h5>
+                                    <p class="card-text">Total Amount of calories: {Math.round(data.recipe.calories)} </p>
+                                    <a href='https://www.edamam.com/results/recipes/?search=all' class="btn btn-primary">Buy</a>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
             </div>
-            )}  
-                     
-        </div>    
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Extension;
